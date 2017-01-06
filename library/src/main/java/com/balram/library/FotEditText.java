b9e7ed6670f59b4f60f293db436b848f05204c74
Balram Pandey <balrampandey19@gmail.com>
2017-01-06 16:37:03 +0530

    FotTextView

--- library/src/main/java/com/balram/library/FotEditText.java
+++ library/src/main/java/com/balram/library/FotEditText.java
@@ -1,8 +1,50 @@
 package com.balram.library;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
 /**
  * Created by Balram Pandey on 1/6/17.
  */
 
-public class FotEditText {
+public class FotEditText  extends TextView {
+    private static final String TAG = "FotTextView";
+
+    public FotEditText(Context context) {
+        super(context);
+    }
+
+    public FotEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setCustomFont(context, attrs);
+    }
+
+    public FotEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setCustomFont(context, attrs);
+    }
+
+    private void setCustomFont(Context ctx, AttributeSet attrs) {
+        TypedArray a = ctx.obtainStyledAttributes(attrs, R.styleable.Fot);
+        String customFont = a.getString(R.styleable.Fot_font);
+        setCustomFont(ctx, customFont);
+        a.recycle();
+    }
+
+    public boolean setCustomFont(Context ctx, String asset) {
+        Typeface typeface = null;
+        try {
+            typeface = Typeface.createFromAsset(ctx.getAssets(), asset);
+        } catch (Exception e) {
+            Log.e(TAG, "Unable to load typeface: "+e.getMessage());
+            return false;
+        }
+
+        setTypeface(typeface);
+        return true;
+    }
 }
